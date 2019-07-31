b97d002e5c5eb33284a42b9cbed364032dd2d1ad
David Harting <davideharting@gmail.com>
2019-07-30 21:04:06 -0400

    Display users and shouts

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,4 +3,12 @@ class Shout < ApplicationRecord
 
   validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
+
+  # This is a bit controversial to use a default scope here
+  # It can be difficult to "undo"
+  # However, we aren't modifying too much here
+  # And ActiveRecord now has a "reorder" feature
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
