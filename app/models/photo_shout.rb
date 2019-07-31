5731b31936c31ed7ba7f0ed192055429360161ac
David Harting <davideharting@gmail.com>
2019-07-31 17:02:03 -0400

    document lack of validation

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,3 +1,7 @@
 class PhotoShout < ApplicationRecord
   has_one_attached :image
+
+  # Apparently activate storage validations exist in Rails 6 but
+  # I am struggling to find documentation
+  # I would like to validate content type and file size
 end
