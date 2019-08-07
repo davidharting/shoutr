53db3c86ee763242855749390b2658b4aa57eddc
David Harting <davideharting@gmail.com>
2019-08-07 16:24:23 -0400

    Prevent errors when guest attempts to like a shout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+  protect_from_forgery with: :exception
+
+  # We are trying to elegantly handle unauthenticated users attempting to do things
+  # We do not really want to have to check "is signed in" on every controller action
+  # So we instead provide this Guest user as the signed in user
+  # And make it behave appropriately.
+  # Note though that we still need the before_action :require_login on controllers
+  def current_user
+    super || Guest.new
+  end
 end
