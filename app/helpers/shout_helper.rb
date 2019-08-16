bfce3ad2059303531444afba87a3bf37d8ff1aca
David Harting <davideharting@gmail.com>
2019-08-16 18:52:19 -0400

    [refactor] Abstract all but content type handling from shout form

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -11,4 +11,12 @@ module ShoutHelper
     subbed = text.gsub(/@\w*/) { |mention| link_to mention, user_path(mention[1..-1]) }
     subbed.html_safe
   end
+
+  def shout_form_for(content_type)
+    form_for Shout.new do |form|
+      form.hidden_field(:content_type, value: content_type) + 
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit("Shout!")
+    end
+  end
 end
